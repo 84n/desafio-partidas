84d191fa488ced7797206b0d98e636cd62907909
84n <zicadarave@gmail.com>
2023-10-27 10:42:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function partidas(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (saldoVitorias < 10) {
+        nivel = 'Ferro';
+    } else if (saldoVitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (saldoVitorias <= 50) {
+        nivel = 'Prata';
+    } else if (saldoVitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (saldoVitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (saldoVitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+let vitorias = 666;  
+let derrotas = 0; 
+
+let resultado = partidas(vitorias, derrotas);
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`);
